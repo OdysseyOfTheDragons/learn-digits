a3c9f6ceeab52a964ad17de2685c4c3b3c3f3eef
alexandre <odysseyofthedragons@gmail.com>
2025-02-14 22:32:12 +0100

    [add] Path to assets

---
+++ include/assets.h
@@ -0,0 +1,14 @@
+/** Provides the path to the assets files. */
+
+#ifndef _ASSETS_H
+#define _ASSETS_H
+/** Defines the root of the application (from within the `src` directory). */
+#define ROOT "../"
+
+const char* path_e = ROOT "./assets/e.txt";
+const char* path_euler = ROOT "./assets/euler.txt";
+const char* path_ln2 = ROOT "./assets/ln2.txt";
+const char* path_phi = ROOT "./assets/phi.txt";
+const char* path_pi = ROOT "./assets/pi.txt";
+const char* path_sqrt2 = ROOT "./assets/sqrt2.txt";
+#endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "assets.h"
+
 int main(void) {
 	printf("Hello, World!\n");
+	printf("%s\n", path_pi);
 	return EXIT_SUCCESS;
 }
