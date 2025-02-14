88dce5d9d3f7d6c9dde75f6d13db22cf65b28751
alexandre <odysseyofthedragons@gmail.com>
2025-02-14 23:53:41 +0100

    [update] Working CLI but with UI bugs

--- src/main.c
+++ src/main.c
@@ -10,11 +10,42 @@ int main(void) {
 	// The CLI tool only supports the digits of pi initially.
 	FILE* file = file_open(path_pi);
 
-	char buffer[length + 1] = { 0 };
-	fgets(buffer, length, file);
+	printf("# Learn digits #\n");
+
+	int buffer = fgetc(file);
+
+	printf(">>> ");
+	while (buffer != EOF && buffer != '.') {
+		printf("%c", buffer);
+		buffer = fgetc(file);
+	}
+	printf(".\n\n");
+
+	size_t cursor = 1;
+	buffer = fgetc(file);
+
+	char user_input;
+	while (buffer != EOF) {
+	   printf("(%d) ", cursor);
+
+	   user_input = getchar();
+
+	   // If there is a line feed, skip it.
+	   if (user_input == 10) {
+		   continue;
+	   }
+
+	   // If the digit is the wrong one, stop the game.
+	   if (buffer != user_input) {
+		   printf("Too bad! %d-th digit was %d, not %d.\n",
+				   cursor, buffer - '0', user_input - '0');
+		   return EXIT_SUCCESS;
+	   }
+
+	   cursor++;
+	   buffer = fgetc(file);
+	}
+
 	file_close(file);
-
-	printf("%s\n", buffer);
-
 	return EXIT_SUCCESS;
 }
